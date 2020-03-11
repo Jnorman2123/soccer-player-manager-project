6a4dfaa680bfb6b5b63dbb415dffc61437e28250
Jnorman2123 <Jnorman2123@gmail.com>
2020-03-11 15:26:53 -0400

    added root reducer to combine reducers

---
+++ src/reducers/rootReducer.js
@@ -0,0 +1,14 @@
+import { combineReducers } from 'redux'
+import { connectRouter } from 'connected-react-router'
+import playerReducer from 'playerReducer'
+import teamReducer from 'teamReducer'
+
+
+const rootReducer = (history) => combineReducers({
+    router: connectRouter(history),
+    teams: teamReducer,
+    players: playerReducer
+
+})
+
+export default rootReducer
