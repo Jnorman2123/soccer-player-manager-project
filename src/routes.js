30f9efd3c556862247563f8c7c2166a86fececac
Jnorman2123 <Jnorman2123@gmail.com>
2020-03-11 16:26:56 -0400

    added routes for players and home to routes

--- src/routes.js
+++ src/routes.js
@@ -1,13 +1,16 @@
 import React from 'react'
 import { Route, Switch } from 'react-router'
 import Teams from './components/teams/Teams'
+import Players from './components/players/Players'
 import NavBar from './components/nav/NavBar'
 
 const routes = (
     <div>
         <NavBar />
         <Switch>
+            {/* <Route exact path='/' component={Home} />} */}
             <Route exact path='/teams' component={Teams} />
+            <Route exact path='/players' component={Players} />
         </Switch>
     </div>
 )
