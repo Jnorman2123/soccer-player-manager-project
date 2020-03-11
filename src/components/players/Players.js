a93d76a6247760a47b1bff7ad392fe8252dae5d7
Jnorman2123 <Jnorman2123@gmail.com>
2020-03-11 12:41:27 -0400

    made basic player component and imported to players

--- src/components/players/Players.js
+++ src/components/players/Players.js
@@ -1,10 +1,12 @@
 import React, { Component } from 'react' 
+import Player from './Player'
 
 class Players extends Component {
     render() {
         return (
             <div>
                 Players
+                <Player />
             </div>
         )
     }
