945b04ec9b992786d4a8b9d2387bd345cb2e2df8
Jnorman2123 <Jnorman2123@gmail.com>
2020-03-11 14:53:12 -0400

    added some seed data for team and player

--- project-backend/db/seeds.rb
+++ project-backend/db/seeds.rb
@@ -6,3 +6,8 @@
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
 
+teams = Team.create([{ name: 'Atlanta United', formation: '3-4-3', salary_cap: 75000000},
+name: 'Chelsea FC', formation: '4-3-3', salary_cap: 1000000000])
+
+players = Player.create([{ name: 'Josef Martinez', position: 'Forward', value: 19250000},
+name: 'Christian Pulisic', position: 'Forward', value: 66600000])
