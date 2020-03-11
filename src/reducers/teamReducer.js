a01b47522604ad353e8db89ecd2e5771a4b8b924
Jnorman2123 <Jnorman2123@gmail.com>
2020-03-11 17:21:26 -0400

    added two cases to team reducer for fetch request

--- src/reducers/teamReducer.js
+++ src/reducers/teamReducer.js
@@ -1,5 +1,17 @@
-export default function teamReducer(state = { teams: [] }, action) {
+export default function teamReducer(state = { teams: [], requesting: false }, action) {
     switch (action.type) {
+        case 'START_ADDING_TEAMS_REQUEST':
+            return {
+                ...state,
+                teams: [...state.teams],
+                requesting: true
+            }
+        case 'ADD_TEAMS':
+            return {
+                ...state,
+                teams: action.teams,
+                requesting: false
+            }
         case 'ADD_TEAM': 
             return {
                 ...state,
