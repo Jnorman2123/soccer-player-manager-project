3e7979a3b08cf64dfdfd54883d71db0970590d1f
Jnorman2123 <Jnorman2123@gmail.com>
2020-03-11 19:06:27 -0400

    added fetch reducers to players reducer

--- src/reducers/playerReducer.js
+++ src/reducers/playerReducer.js
@@ -1,5 +1,17 @@
 export default function playerReducer( state = { players: [] }, action) {
     switch (action.type) {
+        case 'START_ADDING_PLAYERS_REQUEST':
+            return {
+                ...state,
+                players: [...state.players],
+                requesting: true
+            }
+        case 'ADD_PLAYERS':
+            return {
+                ...state,
+                players: action.players,
+                requesting: false
+            }
         case 'ADD_PLAYER':
             return {
                 ...state,
