0835ac852c64286bc3f3c3606e6ba6ac0b5d009a
Jnorman2123 <Jnorman2123@gmail.com>
2020-03-11 12:08:26 -0400

    added very basic team container component

---
+++ src/containers/TeamContainer.js
@@ -0,0 +1,13 @@
+import React, { Component } from 'react'
+
+class TeamContainer extends Component {
+    render() {
+        return (
+            <div>
+                Teams Container
+            </div>
+        )
+    }
+}
+
+export default TeamContainer
