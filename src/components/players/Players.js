6d79047136bacb12844a92153578bb0b51ee0756
Jnorman2123 <Jnorman2123@gmail.com>
2020-03-11 12:38:53 -0400

    created basic players component and imported to player container

---
+++ src/components/players/Players.js
@@ -0,0 +1,13 @@
+import React, { Component } from 'react' 
+
+class Players extends Component {
+    render() {
+        return (
+            <div>
+                Players
+            </div>
+        )
+    }
+}
+
+export default Players 
--- src/containers/PlayerContainer.js
+++ src/containers/PlayerContainer.js
@@ -1,10 +1,12 @@
-import React, { Component } from 'react' 
+import React, { Component } from 'react'
+import Players from '../components/players/Players' 
 
 class PlayerContainer extends Component {
     render () {
         return (
             <div>
                 Player Container
+                <Players />
             </div>
         )
     }
