9d9f90e63033555b8c813bc53db1ac1ed91a18fe
Jnorman2123 <Jnorman2123@gmail.com>
2020-03-12 20:31:53 -0400

    added basic inputs for formation and salary cap to team form

--- src/components/teams/TeamForm.js
+++ src/components/teams/TeamForm.js
@@ -4,9 +4,14 @@ class TeamForm extends Component {
     render() {
         return (
             <div>
+                <h3>Create a New Team</h3>
                 <form>
-                    <label>Team Form</label>
-                    <input type='text'></input>
+                    <label>Name: </label>
+                    <input name='name' type='text'></input><br></br>
+                    <label>Formation: </label>
+                    <input name='formation' type='text'></input><br></br>
+                    <label>Salary Cap: </label>
+                    <input name='salary_cap' type='text'></input><br></br>
                     <input type='submit' value='Create Team'></input>
                 </form>
             </div>
