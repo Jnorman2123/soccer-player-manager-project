d3bef1bb4282219ff446a5bcff828cdbdd8919f1
Jnorman2123 <Jnorman2123@gmail.com>
2020-03-14 12:29:30 -0400

    completed new player form

--- src/components/players/PlayerForm.js
+++ src/components/players/PlayerForm.js
@@ -1,12 +1,43 @@
 import React, { Component } from 'react' 
+import { connect } from 'react-redux'
+import { addPlayer } from '../../actions/players/playerActions'
 
 class PlayerForm extends Component {
+    state = {
+            name: '',
+            position: '',
+            value: ''
+    }
+    
+    
+    handleChange = (event) => {
+        this.setState({
+            [event.target.name]: event.target.value
+        })
+    }
+s
+    handleSubmit = (event) => {
+        event.preventDefault()
+        this.props.addPlayer(this.state)
+        this.setState({
+            name: '',
+            position: '',
+            value: ''
+        })
+        this.props.history.push('/players')
+    }
+
     render() {
         return (
             <div>
-                <form>
-                    <label>Player Form</label>
-                    <input type='text'></input>
+                <h3>Create a New Player</h3>
+                <form onSubmit={this.handleSubmit}> 
+                    <label>Name: </label>
+                    <input name='name' type='text' onChange={this.handleChange} value={this.state.name}></input><br></br>
+                    <label>Position: </label>
+                    <input name='position' type='text' onChange={this.handleChange} value={this.state.position}></input><br></br>
+                    <label>Value: </label>
+                    <input name='value' type='number' onChange={this.handleChange} value={this.state.value}></input><br></br>
                     <input type='submit' value='Create Player'></input>
                 </form>
             </div>
@@ -14,4 +45,4 @@ class PlayerForm extends Component {
     }
 }
 
-export default PlayerForm
+export default connect(null, { addPlayer })(PlayerForm)
