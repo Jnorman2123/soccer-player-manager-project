3cf97f251562f6f2416739227d53a7672dd25ae2
Jnorman2123 <Jnorman2123@gmail.com>
2020-03-14 11:42:20 -0400

    added logic to player container component

--- src/containers/PlayerContainer.js
+++ src/containers/PlayerContainer.js
@@ -1,13 +1,42 @@
 import React, { Component } from 'react'
+import { connect } from 'react-redux'
+import { fetchPlayers, deletePlayer } from '../actions/players/playerActions'
+import { Link } from 'react-router-dom'
+import Player from '../components/Players/Player'
 
 class PlayerContainer extends Component {
+
+    componentDidMount() {
+        this.props.fetchPlayers()
+    }
+
+    renderPlayer() {
+        const playerId = this.props.match.params.playerID
+        const thePlayer = this.props.players.players.filter(player => player.id === parseInt(playerId))[0]
+        if (thePlayer !== undefined) {
+            return <div>
+                <h2>{thePlayer.name}</h2>
+                <h3>{thePlayer.position}</h3>
+                <h4>{thePlayer.name} has a value of  ${thePlayer.value.toString()}.</h4>
+                <button onClick={() => {this.props.deletePlayer(playerId); this.props.history.push('/players')} }>Delete {thePlayer.name}</button>
+                <Link to={{pathname: `/players/${playerId.toString()}/edit`, player: thePlayer }  }>
+                        <button>Edit {thePlayer.name}</button>
+                </Link>
+            </div>
+        }
+    }
+
     render() {
         return (
             <div>
-                Player Container
+                <Player props={this.props} renderPlayer={this.renderPlayer} />
             </div>
         )
     }
 }
 
-export default PlayerContainer
+const mapStateToProps = (state) => {
+    return {players: state.players}
+}
+
+export default connect(mapStateToProps, { fetchPlayers, deletePlayer })(PlayerContainer)
