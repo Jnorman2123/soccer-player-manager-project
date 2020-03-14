73f1c4a10039c60a98d313098712d24fd3f41957
Jnorman2123 <Jnorman2123@gmail.com>
2020-03-14 10:45:13 -0400

    moved all logic out of team file into team container

--- src/components/teams/Team.js
+++ src/components/teams/Team.js
@@ -1,7 +1,4 @@
 import React, { Component } from 'react' 
-import { connect } from 'react-redux'
-import { fetchTeams, deleteTeam } from '../../actions/teams/teamActions'
-import { Link } from 'react-router-dom'
 
 class Team extends Component {
     
@@ -10,43 +7,15 @@ class Team extends Component {
         this.props.fetchTeams()
     }
 
-    handleClick = () => {
-        const id = this.props.match.params.teamID
-        this.props.deleteTeam(id)
-        this.props.history.push('/teams')
-    }
-
-    renderTeam() {
-        const teamId = parseInt(this.props.match.params.teamID)
-        const theTeam = this.props.teams.teams.filter(team => team.id === teamId)[0]
-        if (theTeam !== undefined) {
-            return <div>
-                <h2>{theTeam.name}</h2>
-                <h3>{theTeam.formation}</h3>
-                <h4>{theTeam.name} has ${theTeam.salary_cap.toString()} remaining salary cap balance.</h4>
-                <button onClick={this.handleClick}>Delete {theTeam.name}</button>
-                <Link to={{pathname: `/teams/${teamId.toString()}/edit`, team: theTeam }  }>
-                        <button>Edit {theTeam.name}</button>
-                </Link>
-            </div>
-        }
-    }
-
-    render() {
-        
-        
+    render() {      
         return (
             <div>
                 Team<br></br>
-                {this.renderTeam()}
+                {this.props.renderTeam()}
             </div>
         )
         
     }   
 } 
 
-const mapStateToProps = (state) => {
-    return {teams: state.teams}
-}
-
-export default connect(mapStateToProps, { fetchTeams, deleteTeam })(Team)
+export default Team
--- src/containers/TeamContainer.js
+++ src/containers/TeamContainer.js
@@ -1,14 +1,39 @@
 import React, { Component } from 'react'
 import { connect } from 'react-redux'
+import { fetchTeams, deleteTeam } from '../actions/teams/teamActions'
+import { Link } from 'react-router-dom'
+import Team from '../components/teams/Team'
 
 class TeamContainer extends Component {
+
+    renderTeam() {
+        const teamId = this.props.match.params.teamID
+        const theTeam = this.props.teams.teams.filter(team => team.id === parseInt(teamId))[0]
+        if (theTeam !== undefined) {
+            return <div>
+                <h2>{theTeam.name}</h2>
+                <h3>{theTeam.formation}</h3>
+                <h4>{theTeam.name} has ${theTeam.salary_cap.toString()} remaining salary cap balance.</h4>
+                <button onClick={() => {this.props.deleteTeam(teamId); this.props.history.push('/teams')} }>Delete {theTeam.name}</button>
+                <Link to={{pathname: `/teams/${teamId.toString()}/edit`, team: theTeam }  }>
+                        <button>Edit {theTeam.name}</button>
+                </Link>
+            </div>
+        }
+    }
+
     render() {
         return (
             <div>
                 Team Container
+                <Team renderTeam={this.renderTeam} fetchTeams={this.props.fetchTeams} props={this.props} deleteTeam={this.props.deleteTeam} />
             </div>
         )
     }
 }
 
-export default connect()(TeamContainer)
+const mapStateToProps = (state) => {
+    return {teams: state.teams}
+}
+
+export default connect(mapStateToProps, { fetchTeams, deleteTeam })(TeamContainer)
