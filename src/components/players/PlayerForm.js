e9ea1c2108539dee27fef7e4890a163185607878
Jnorman2123 <Jnorman2123@gmail.com>
2020-03-15 12:59:02 -0400

    added drop down for position to create player form

--- src/components/players/PlayerForm.js
+++ src/components/players/PlayerForm.js
@@ -34,8 +34,15 @@ s
                 <form onSubmit={this.handleSubmit}> 
                     <label>Name: </label>
                     <input name='name' type='text' onChange={this.handleChange} value={this.state.name}></input><br></br>
-                    <label>Position: </label>
-                    <input name='position' type='text' onChange={this.handleChange} value={this.state.position}></input><br></br>
+                    <label>
+                        Select Position:
+                        <select name='position' value={this.state.position} onChange={this.handleChange}>
+                            <option value='Forward'>Forward</option>
+                            <option value='Midfielder'>Midfielder</option>
+                            <option value='Defender'>Defender</option>
+                            <option value='Goalkeeper'>Goalkeeper</option>
+                        </select> 
+                    </label><br></br>
                     <label>Value: </label>
                     <input name='value' type='number' onChange={this.handleChange} value={this.state.value}></input><br></br>
                     <input type='submit' value='Create Player'></input>
