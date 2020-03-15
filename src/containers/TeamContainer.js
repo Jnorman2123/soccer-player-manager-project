d8317d20aea3c5f599c7d50f58ee6edc2bde3c06
Jnorman2123 <Jnorman2123@gmail.com>
2020-03-15 15:01:45 -0400

    changed team render

--- src/containers/TeamContainer.js
+++ src/containers/TeamContainer.js
@@ -19,12 +19,16 @@ class TeamContainer extends Component {
         } else if (theTeam.players !== undefined && theTeam.players.length > 0) {
             return <div>
                 <h3>Players</h3>
+                <h4>Forwards</h4>
                 {theTeam.players.map((player, i) => {
-                return <div key={i}>
+                if (player.position === 'Forward') {
+                    return <div key={i}>
                     <h3>{player.name}</h3>
-                    <h4>{player.position}</h4>
                 </div>
-            })}
+            }})}
+            <Link to={{pathname: `/teams/${theTeam.id}/transfer`, team: theTeam }  }>
+                        <button>Add Player to {theTeam.name}</button>
+            </Link>
             </div>
         }
     }
@@ -34,11 +38,6 @@ class TeamContainer extends Component {
             return <div>
                 <h2>{theTeam.name}</h2>
                 <h3>{theTeam.formation}</h3>
-                
-                
-                <Link to={{pathname: `/teams/${theTeam.id}/transfer`, team: theTeam }  }>
-                        <button>Add Player to {theTeam.name}</button>
-                </Link>
                 <h4>{theTeam.name} has ${theTeam.salary_cap} remaining salary cap balance.</h4>
                 <button onClick={() => {this.props.deleteTeam(theTeam.id); this.props.history.push('/teams')} }>Delete {theTeam.name}</button>
                 <Link to={{pathname: `/teams/${theTeam.id}/edit`, team: theTeam }  }>
