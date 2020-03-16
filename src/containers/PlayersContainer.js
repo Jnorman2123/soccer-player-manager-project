0cb314f0ca9353ac1a5a26d6bab4a3a177d5eeda
Jnorman2123 <Jnorman2123@gmail.com>
2020-03-16 16:22:46 -0400

    add styling to players page

--- src/containers/PlayersContainer.js
+++ src/containers/PlayersContainer.js
@@ -11,21 +11,27 @@ class PlayersContainer extends Component {
     }
 
     renderPlayers(position) {
-        return <div name={position}>
-            <h1>{position}s</h1>
-            {this.props.players.players.map((player, i) => {
-                if (player.position === position) {
-                    return <Link to={`/players/${player.id}`} key={i}>
-                        <p>{player.name} - Value: ${player.value}</p>
-                    </Link>
-                } 
-            })}
+        return <div class='table' name={position}>
+            <h2>{position}s</h2>
+            <table class='w3-table-all w3-card-4'>
+                {this.props.players.players.map((player, i) => {
+                    if (player.position === position) {
+                        return <tr key={i}>
+                            <Link to={`/players/${player.id}`}>
+                                <td>{player.name}</td>
+                            </Link>
+                            <td>Value: ${player.value}</td>
+                        </tr>
+                    } 
+                })}
+            </table>
         </div>
     }
 
     render () {
         return (
-            <div>
+            <div class='team'>
+                <h1>Players</h1>
                 <Players props={this.props} renderPlayers={this.renderPlayers} />
             </div>
         )
